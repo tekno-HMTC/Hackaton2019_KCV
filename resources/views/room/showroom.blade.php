d025757192194989ddf61a9f7f0ade2c9aa3c181
yolandahp <yolandahertita903@gmail.com>
2019-05-04 22:55:16 +0700

    capeq

--- resources/views/room/showroom.blade.php
+++ resources/views/room/showroom.blade.php
@@ -5,7 +5,7 @@
             background-color: #fff;
             height: 100vh;
             font-family: 'Roboto+Condensed', sans-serif;
-            background: url('/images/bg4.jpg');
+            background: url('/images/bg5.jpg');
             background-size:     cover;
             background-repeat:   no-repeat;
             background-position: center center;   
@@ -22,7 +22,7 @@
             bottom: 0;
             height: 40vh;
             width: 100vw;
-            background-color: #DD0A00;
+            background-color: #EF4A4E;
         }
 
         .kotak-soal-2 {
@@ -30,7 +30,7 @@
             bottom: 0;
             height: 34vh;
             width: 100vw;
-            background-color: #65171D;
+            background-color: #C93E43;
             margin-bottom: 3vh;
         }
 
@@ -39,7 +39,7 @@
             bottom: 0;
             height: 28vh;
             width: 94vw;
-            background-color: #FFFFFF;
+            background-color: #EAE0D4;
             margin-bottom: 3vh;
             left: 50%;
             margin-left: -47vw;
@@ -52,11 +52,62 @@
         }
 
         .kotak-soal-3 .pilihan-jawaban {
-            padding: 10px 300px;
+            padding: 0px 300px;
             text-align: center;
+            width: 100%;
+            display: inline-table;
         }
 
+        .pilihan-jawaban .jawaban{
+            width: 50%;
+            text-align: center;
+            display: inline-block;
+            height: 50px;
+            line-height: 50px;
+            margin-bottom: 15px;
+        }
+
+        .jawaban p{
+            background-color: #EAE0D4;
+            border: 2px solid rgba(139, 139, 139, .3);
+            border-radius: 25px;
+            margin: 3px 0px;
+            -webkit-touch-callout: none;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+            -webkit-tap-highlight-color: transparent;
+            transition: all .2s;
+
+            transition: transform .3s ease-in-out;
+        }
+
+        .jawaban.clicked p{
+            background-color: #FFF7A5;
+        }
         
+        #kotak-time-bar {
+            position: absolute;
+            top: 0;
+            height: 5vh;
+            width: 50vw;
+            background-color: #EAE0D4;
+            left: 50%;
+            margin-left: -25vw;
+            border-radius: 5px;
+        }
+
+        #kotak-time-bar div {
+            height: 100%;
+            text-align: right;
+            padding: 0 10px;
+            line-height: 5vh;
+            width: 0;
+            background-color: #EF4A4E;
+            box-sizing: border-box;
+        }
+
     </style>
 @endsection
 
@@ -64,35 +115,80 @@
     <div class="main">
         <div class="container text-center">
         </div>
-    
-        <div class="kotak-soal-1">
+            <div id="kotak-time-bar">
+                <div></div>
+            </div>
 
-            <div class="kotak-soal-2">
-                <div class="kotak-soal-3">
-                    <div class="soal">
-                        <h5><b>Siapakah nama saya?</b></h5>
-                    </div>
+            <div class="kotak-soal-1">
 
-                    <div class="pilihan-jawaban">
-                        <div class="jawaban">
-                            Dandy
+                <div class="kotak-soal-2">
+                    <div class="kotak-soal-3">
+                    
+                    <form method="" action="" id="jawab-soal">
+                    @csrf
+                    
+                        <div class="soal">
+                            <h5><b>Siapakah nama saya?</b></h5>
                         </div>
-                        <div class="jawaban">
-                            Randi 
+
+                        <div class="pilihan-jawaban">
+                            <div class="jawaban">
+                                <p>Dandy</p>
+                            </div>
+                            <div class="jawaban">
+                                <p>Randi</p> 
+                            </div>
+                            <div class="jawaban">
+                                <p>Titut</p>
+                            </div>
+                            <div class="jawaban">
+                                <p>Syavira</p>
+                            </div>
                         </div>
-                        <div class="jawaban">
-                            Titut
-                        </div>
-                        <div class="jawaban">
-                            Syavira
-                        </div>
+                    
+                    </form>
+
                     </div>
                 </div>
             </div>
-        </div>
+        
+
     </div>
 
 @endsection
 
 @section('js')
+    <script>
+        $(function(){ 
+            var flag = 0;
+
+            $('.jawaban').click(function(){ 
+                if(!flag){
+                    $(this).addClass('clicked');
+                    
+                    flag = 1;
+                }
+                 
+            }) 
+        });
+
+        function progress(timeleft, timetotal, $element) {
+            var progressBarWidth = timeleft * $element.width() / timetotal;
+            
+            $('#kotak-time-bar div').animate(
+                { width: progressBarWidth + 'px'}, 
+                timeleft == timetotal ? 0 : 1000, 'linear').html(timeleft);
+            if(timeleft > 0) {
+                
+                setTimeout(function() {
+                    progress(timeleft - 1, timetotal, $element);
+                }, 1000);
+                
+            }
+        };
+
+        progress(15, 15, $('#kotak-time-bar'));
+
+    </script>
+
 @endsection
